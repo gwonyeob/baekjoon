9d850d33e75415c25dbd01be5096caf3e23de8fc
G Y <97574449+apircity@users.noreply.github.com>
2022-04-24 14:15:21 +0900

    [Bronze IV] Title: 오븐 시계, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.cc
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int H, M, S, h, m, f;
+    scanf("%d %d", &H, &M);
+    scanf("%d", &S);
+    h = (S+M) /60;
+    m = (S+M) % 60;
+    f = H + h;
+    f = f%24;
+    printf("%d %d", f, m);
+    return 0;
+}

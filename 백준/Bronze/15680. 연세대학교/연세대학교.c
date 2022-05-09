e338412147d61389ede143e3e7c097d830179aeb
G Y <97574449+apircity@users.noreply.github.com>
2022-05-09 17:26:10 +0900

    [Bronze IV] Title: 연세대학교, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n=0;
+    scanf("%d", &n);
+    if(n==0)
+    {
+        printf("YONSEI");
+    }
+    else if(n==1)
+    {
+        printf("Leading the Way to the Future");
+    }
+}

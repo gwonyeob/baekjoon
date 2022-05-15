1d73af169c97eef0c66bbcc2aa2835f13fa0e9ff
G Y <97574449+apircity@users.noreply.github.com>
2022-05-16 00:09:16 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+	int n = 0;
+	scanf("%d", &n);
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = n - i; j > 0; j--)
+			printf(" ");
+		for (int k = 2 * i - 1; k > 0; k--)
+			printf("*");
+		printf("\n");
+	}
+	return 0;
+}

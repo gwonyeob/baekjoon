3ebe191900415603543f711842db9c3b03a1761c
G Y <97574449+apircity@users.noreply.github.com>
2022-05-16 00:12:29 +0900

    [Bronze II] Title: 정수 N개의 합, Time: 4 ms, Memory: 12832 KB -BaekjoonHub

--- 백준/Bronze/15596. 정수 N개의 합/정수 N개의 합.c
+++ 백준/Bronze/15596. 정수 N개의 합/정수 N개의 합.c
@@ -1,6 +1,7 @@
 long long sum(int *a, int n)
 {
-	long long ans = 0;
-	for(int i=0; i<n; i++) ans += a[i];
-    return ans;
+	long long sum = 0;
+	for(int i=0; i<n; i++) 
+        sum += a[i];
+    return sum;
 }

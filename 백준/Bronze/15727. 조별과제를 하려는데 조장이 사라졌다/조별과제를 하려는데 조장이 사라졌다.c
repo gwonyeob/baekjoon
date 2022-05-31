f3f0ce74224bd9c6000914e8aac09c5eed671894
G Y <97574449+apircity@users.noreply.github.com>
2022-06-01 01:54:09 +0900

    [Bronze V] Title: 조별과제를 하려는데 조장이 사라졌다, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/15727. 조별과제를 하려는데 조장이 사라졌다/조별과제를 하려는데 조장이 사라졌다.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int L, t;
+    scanf("%d", &L);
+    t = L;
+    for (int i = 0; i < L; i++) {
+        if (t > 5)
+            t -= 5;
+        else {
+            printf("%d", i + 1);
+            break;
+        }
+    }
+}
